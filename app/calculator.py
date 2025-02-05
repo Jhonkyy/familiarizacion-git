0dcdad0b9554f19577777938cb461550e6325059
Jhon Asprilla <Jhonfre0212@gmail.com>
2025-02-05 13:18:44 -0500

    implemented multiply method

--- app/calculator.py
+++ app/calculator.py
@@ -2,5 +2,8 @@ class Calculator:
     def add(self, a, b):
         return a+b
 
-    def subtraction(self, a,b):
+    def subtraction(self, a, b):
+        return a-b
+
+    def multiply(self, a, b):
         return a-b
